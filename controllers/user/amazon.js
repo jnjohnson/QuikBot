4a6b91ceaaf27b9f27504a4f78e01906661b8055
jimmy johnson <jimmy@jnjohnson.io>
2018-01-09 23:16:02 -0500

    remove console.log statements from testing; added correct hardcoding to access JSON values returned from API call

--- controllers/user/amazon.js
+++ controllers/user/amazon.js
@@ -13,24 +13,28 @@ amazonMethods.searchForItem = function(itemName){
         SearchIndex: 'VideoGames',
         Title: itemName
     }).then(function(results){
-        console.log(results[0]);
-        console.log(results[0].ItemAttributes.Title);
-        console.log(results[0].DetailPageURL[0]);
-        console.log(results[0].MediumImage[0]["URL"][0]);
+        var html = '';
+        for (i=0; i<= results.length-1; i++){
+            html += amazonMethods.createProductThumbnail(results[i]);
+        }
+        return "html";
     }).catch(function(err){
         console.log(err);
+        return "failure";
     });
+    return "hello";
 };
 
 amazonMethods.createProductThumbnail = function(result){
     var html =  '<div class="product-box"><a target="_blank" href=' + result.DetailPageURL[0] + '>' +
                 '<img src=' + result.MediumImage[0]["URL"][0] + '></a><div class="product-title">'+
-                '<h3>' + result.ItemAttributes.Title[0] + '</h3></div><p class="product-price">'+ result.OfferSummary.LowestNewOffer[0] +'<br>'+
+                '<h3>' + result.ItemAttributes[0]["Title"][0] + '</h3></div><p class="product-price">'+ result.OfferSummary[0].LowestNewPrice[0].FormattedPrice[0] +'<br>'+
                 '</p><div><span class="a-button a-button-primary"><a target="_blank" href=' + result.DetailPageURL[0] + 
                 'style="text-decoration:none"><span class="a-button-inner">'+
                 '<img src="http://webservices.amazon.com/scratchpad/assets/images/Amazon-Favicon-64x64.png" class="a-icon a-icon-shop-now">'+
                 '<input class="a-button-input" type="submit" value="Add to cart"><span class="a-button-text">Shop Now</span>'+
                 '</span></a></span></div></div>';
+    return html;
 }
 
-exports.data = amazonMethods;
+module.exports = amazonMethods;
