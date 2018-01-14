086a521cb370a918323dd1efbe376372a78ea2ef
jimmy johnson <jimmy@jnjohnson.io>
2018-01-14 01:05:33 -0500

    introduce changes to HTML that will display search resultsy

--- controllers/user/amazon.js
+++ controllers/user/amazon.js
@@ -24,10 +24,10 @@ amazonMethods.createProductThumbnails = function(res, itemName){
 }
 
 amazonMethods.generateHTML = function(result){
-    var html =  '<div class="product-box"><a target="_blank" href=' + result.DetailPageURL[0] + '>' +
-                '<img src=' + result.MediumImage[0]["URL"][0] + '></a><div class="product-title">'+
+    var html =  '<div class="product-box col-12 col-md-5 col-lg-4 col-xl-2"><a target="_blank" href=' + result.DetailPageURL[0] + '>' +
+                '<div class="center-element img-height"><img src=' + result.MediumImage[0]["URL"][0] + '></div></a><div class="product-title">'+
                 '<h3>' + result.ItemAttributes[0]["Title"][0] + '</h3></div><p class="product-price">'+ result.OfferSummary[0].LowestNewPrice[0].FormattedPrice[0] +'<br>'+
-                '</p><div><span class="a-button a-button-primary"><a target="_blank" href=' + result.DetailPageURL[0] + 
+                '</p><div><span class="center-element a-button a-button-primary"><a target="_blank" href=' + result.DetailPageURL[0] + 
                 'style="text-decoration:none"><span class="a-button-inner">'+
                 '<img src="http://webservices.amazon.com/scratchpad/assets/images/Amazon-Favicon-64x64.png" class="a-icon a-icon-shop-now">'+
                 '<input class="a-button-input" type="submit" value="Add to cart"><span class="a-button-text">Shop Now</span>'+
