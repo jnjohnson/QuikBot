709ce4e3aa092a81e0f1850c08196ff37b73afc2
jimmy johnson <jimmy@jnjohnson.io>
2018-01-28 10:01:14 -0500

    moved ajax posts to a separate function and implemented promises for handling them. New functions to hide the back button if on the first page of results, get more search results beyond initial two pages

--- public/js/get-input.js
+++ public/js/get-input.js
@@ -1,10 +1,9 @@
 user = {}
 
 var searchValidation = /^[-\sa-zA-Z0-9]+$/;
-var carouselHTML = '<div id="amznCarousel" class="carousel slide"><div class="carousel-inner">' + 
-'<div class="carousel-item active"></div><div class="carousel-item"><img src="http://placehold.it/1000x592">' +
-'</div><div class="carousel-item"><img src="http://placehold.it/1000x592">' +
-'</div></div><a class="carousel-control-prev" href="#amznCarousel" role="button" data-slide="prev">' +
+var carouselHTML = '<div id="amznCarousel" class="carousel slide" data-ride="false" data-interval="false"><div class="carousel-inner">' + 
+'<div class="carousel-item active"></div><div class="carousel-item"></div>' +
+'</div><a class="carousel-control-prev hidden" href="#amznCarousel" role="button" data-slide="prev">' +
 '<img src="/public/images/back.png" class="prev" aria-hidden="true">' +
 '<span class="sr-only">Previous</span></a><a class="carousel-control-next" href="#amznCarousel" role="button" data-slide="next">' +
 '<img src="/public/images/next-1.png" class="next" aria-hidden="true">' + 
@@ -30,42 +29,77 @@ user.init = function(){
                 createErrorMsg("An error occurred");
             }
             else {
-                var searchResult = sendSearch(itemName, pageNum);
-
-                //Need sendSearch to return a promise as it is an async function
-                //In the success function of the promise, insert carousel code and generated html from amazon,
+                var promise = sendSearch(itemName, pageNum);
                 //generate the next page of search results, and attach an event listener to the 'next' button
 
-                if (searchResult != false){
+                promise.then(function(result){
                     document.getElementById("searchResults").innerHTML = carouselHTML;
-                    document.getElementsByClassName("carousel-item")[0].innerHTML = searchResult;
-                }
+                    document.getElementsByClassName("carousel-item")[0].innerHTML = result;
+
+                    var page1Promise = sendSearch(itemName, pageNum+1);
+
+                    page1Promise.then(function(page1Result){
+                        document.getElementsByClassName("carousel-item")[1].innerHTML = page1Result;
+                        document.getElementsByClassName("carousel-control-next")[0].addEventListener("click", function(){getNewSlide(itemName, ++pageNum)}, false);
+                        document.getElementsByClassName("carousel-control-prev")[0].addEventListener("click", function(){checkPrevSlide(--pageNum)}, false);
+                    }).catch(function(e){
+                        createErrorMsg(e);
+                    });
+
+                }).catch(function(e){
+                    createErrorMsg(e);
+                });
             }
         }, false);
 	}
 }
 
 function sendSearch(itemName, pageNum){
-    var data = {};
-    data.itemName   = itemName;
-    data.pageNum    = pageNum;
+    return new Promise(function(resolve, reject){
+        var data = {};
+        data.itemName   = itemName;
+        data.pageNum    = pageNum;
 
-    data = JSON.stringify(data);
-    Ajax.sendRequest('/', function(res){
-        if (res.responseText == "no item"){
-            createErrorMsg("You need to input an item!");
-        }
-        else if (res.responseText == "invalid input"){
-            createErrorMsg("Invalid characters used in search");
-        }
-        else if (res.responseText == "An error occurred"){
-            createErrorMsg("An error occurred");
-        }
-        else {
-            return res.responseText;
-        }
-        return false;
-    }, data);
+        data = JSON.stringify(data);
+        Ajax.sendRequest('/', function(res){
+            if (res.responseText == "no item"){
+                reject("You need to input an item!");
+            }
+            else if (res.responseText == "invalid input"){
+                reject("Invalid characters used in search");
+            }
+            else if (res.responseText == "An error occurred"){
+                reject("An error occurred");
+            }
+            else {
+                resolve(res.responseText);
+            }
+        }, data);
+    });
+}
+
+function getNewSlide(itemName, newPageNum){
+    if (newPageNum === 2){
+        document.getElementsByClassName("carousel-control-prev")[0].classList.remove("hidden");
+    }
+
+    if (newPageNum == document.getElementsByClassName("carousel-item").length){
+        var promise = sendSearch(itemName, newPageNum+1);
+        promise.then(function(result){
+            var newSlide = document.createElement('div');
+            newSlide.className = "carousel-item";
+            newSlide.innerHTML = result;
+            document.getElementsByClassName("carousel-inner")[0].appendChild(newSlide);
+        }).catch(function(e){
+            createErrorMsg(e);
+        });
+    }
+}
+
+function checkPrevSlide(newPageNum){
+    if (newPageNum === 1){
+        document.getElementsByClassName("carousel-control-prev")[0].classList.add("hidden");
+    }
 }
 
 function createErrorMsg(errorMsg){
